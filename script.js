ad03f48a1e71a14015efdfcd5d20be745f233126
Mohammad <153366997+majedrora9@users.noreply.github.com>
2025-04-21 20:59:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,10 @@ async function fetchAvailableTimes() {
 }
 
 // دالة للتحقق مما إذا كان اليوم هو الاثنين
-
+function isMonday() {
+    const today = new Date();
+    return today.getDay() === 2; // 1 يمثل يوم الاثنين (الأحد هو 0)
+}
 
 // عند تحميل الصفحة
 document.addEventListener('DOMContentLoaded', () => {
